2f7728f5f6fceba22a1384182e26d3daf03840a8
Janaina-Costa <janainacosta.dev@gmail.com>
2024-02-25 00:28:23 -0300

    feat: add throw error no athController

--- src/controllers/Auth.Controller.ts
+++ src/controllers/Auth.Controller.ts
@@ -3,50 +3,47 @@ import { userAuthenticateService } from "services/Auth.Service";
 import { IUser } from "types/interfaces/user";
 import bcrypt from "bcrypt";
 import { SECRET_KEY } from "settings";
+import { BadRequestError } from "errors/ApiErrors";
 
 const secret = SECRET_KEY;
 
 class AuthUserController {
   async loginUserController(req: Request, res: Response) {
-    try {
-      const user: IUser = req.body;
-      const userData = await userAuthenticateService.loginService(user.email);
+    const user: IUser = req.body;
+    const userData = await userAuthenticateService.loginService(user.email);
 
-      if (!user.email || user.email !== userData?.email) {
-        return res.status(400).send({ message: "Email or password invalid" });
-      }
-      let passwordToString;
-
-      passwordToString = String(userData?.password);
-
-      const comparePassword = await bcrypt.compare(
-        user.password,
-        passwordToString,
-      );
-
-      if (!comparePassword) {
-        return res.status(400).send({ message: "Email or password invalid" });
-      }
-
-      const userLogged = {
-        id: userData.id,
-        name: userData.name,
-        email: userData.email,
-      };
-
-      const token = await userAuthenticateService.generateTokenService(
-        userData.id,
-        secret,
-      );
-
-      return res.status(200).send({
-        userLogged,
-        token,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ message: "Internal server error" });
+    if (!user.email || user.email !== userData?.email) {
+      throw new BadRequestError("Email or password invalid");
     }
+    let passwordToString;
+
+    passwordToString = String(userData?.password);
+
+    const comparePassword = await bcrypt.compare(
+      user.password,
+      passwordToString,
+    );
+
+    if (!comparePassword) {
+      throw new BadRequestError("Email or password invalid");
+    }
+
+    const userLogged = {
+      id: userData.id,
+      name: userData.name,
+      email: userData.email,
+    };
+
+    const token = await userAuthenticateService.generateTokenService(
+      userData.id,
+      secret,
+    );
+
+    return res.status(200).send({
+      userLogged,
+      token,
+      message: "User logged in successfully",
+    });
   }
 }
 
